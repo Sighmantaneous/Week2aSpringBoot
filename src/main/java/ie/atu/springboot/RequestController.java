cf3b25331a412e9dc30a5be9738bc3b3f77f198c
sighman <g00286398@atu.ie>
2025-09-24 14:31:36 +0100

    Adding Path variable

--- src/main/java/ie/atu/springboot/RequestController.java
+++ src/main/java/ie/atu/springboot/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.springboot;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class RequestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hello " + name;
     }
 }
